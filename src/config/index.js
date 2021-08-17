d8d46c4310284da790bd4eb8646f9a3b21ca8b90
hidayoan <minuskg123@gmail.com>
2021-08-17 21:20:55 +0700

    project

---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+export const config = {
+  api: {
+    url: 'http://localhost:3000/api/v1'
+  },
+  paypal: {
+    secretKey: null
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyCAu9-RUNbDrxnKpNh2NVsb-lwY4yuiIhw",
+    authDomain: "test-sheets-project-app.firebaseapp.com",
+    databaseURL: "https://test-sheets-project-app-default-rtdb.firebaseio.com",
+    projectId: "test-sheets-project-app",
+    storageBucket: "test-sheets-project-app.appspot.com",
+    messagingSenderId: "537266275370",
+    appId: "1:537266275370:web:987f8b98e1d5d86b776f53",
+    measurementId: "G-PYFG97KCY6"
+  }
+}
